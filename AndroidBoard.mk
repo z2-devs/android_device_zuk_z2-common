0f33141b39835fb61fac0b3c6c292e0d15cff325
HostZero <haneesh.da.rocker@gmail.com>
2016-11-02 21:51:30 +0530

    mustang: cm dosent own generate_extra_images

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -178,7 +178,6 @@ endif
 # extra images
 #----------------------------------------------------------------------
 #ifeq (, $(wildcard vendor/qcom/build/tasks/generate_extra_images.mk))
-include device/qcom/common/generate_extra_images.mk
 #endif
 
 #----------------------------------------------------------------------
