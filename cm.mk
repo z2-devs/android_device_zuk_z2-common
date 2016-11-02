fd010c450b1be5451bff025338d3c52c4db65e63
HostZero <haneesh.da.rocker@gmail.com>
2016-11-02 16:42:36 +0300

    mustang: inherit mustang.mk

--- cm.mk
+++ cm.mk
@@ -17,7 +17,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from mustang device
-$(call inherit-product, device/zuk/mustang/device.mk)
+$(call inherit-product, device/zuk/mustang/mustang.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
