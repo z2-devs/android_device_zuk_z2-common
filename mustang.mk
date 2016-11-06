9071580e4fca5c29c215e6b56baab42f7439639c
HostZero <haneesh.da.rocker@gmail.com>
2016-11-06 16:26:38 +0530

    mustang: remove audio_platform_info_i2s.xml

--- mustang.mk
+++ mustang.mk
@@ -120,7 +120,6 @@ PRODUCT_COPY_FILES += \
     device/zuk/mustang/mixer_paths_tasha_z2_row.xml:system/etc/mixer_paths_tasha_z2_row.xml \
     device/zuk/mustang/mixer_paths_dtp.xml:system/etc/mixer_paths_dtp.xml \
     device/zuk/mustang/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
-    device/zuk/mustang/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
     device/zuk/mustang/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     device/zuk/mustang/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
     device/zuk/mustang/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
