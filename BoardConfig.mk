496c0b80b1b441244db179d938994c3166616749
HostZero <haneesh.da.rocker@gmail.com>
2016-11-12 18:05:47 +0530

    z2-common: Add VibratorHW

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,6 +83,9 @@ ifneq ($(TARGET_USES_AOSP),true)
 TARGET_USES_QCOM_BSP := true
 endif
 
+# CM Hardware
+BOARD_HARDWARE_CLASS += device/zuk/z2-common/cmhw
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
